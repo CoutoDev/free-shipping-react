8fccb2d1b691d3e7a512bd9962168a8819e73c73
CoutoDev <c.svitor@yahoo.com.br>
2021-12-14 19:35:21 -0300

    call everything on app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ProductList from './components/ProductList/ProductList';
+import FreeShipping from './components/FreeShipping/FreeShipping';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [quoteTotals, setQuoteTotals] = useState(0)
+
+    const updateQuoteTotals = (productPrice) => {
+        setQuoteTotals(quoteTotals + productPrice)
+    }
+
+    return (
+        <div className="App">
+            <FreeShipping totalToApplyFreeShipping={150} quoteTotals={quoteTotals}/>
+            <ProductList updateQuoteTotals={updateQuoteTotals}/>
+        </div>
+    );
 }
 
 export default App;
